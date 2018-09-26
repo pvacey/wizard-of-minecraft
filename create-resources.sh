530dd8ea1e022572c2b7dd90e4f1dbd79d30538a
Paul Vacey <pvacey@gmail.com>
2018-09-26 18:24:07 -0500

    prompt for RCON password

--- create-resources.sh
+++ create-resources.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+echo "enter password for RCON:"
+read -s NEW_PASSWORD
+sed -i "s/PLACEHOLDER_PASSWORD/${NEW_PASSWORD}/g" instance.yml
 sed -i "s/PROJECT_ID/${GOOGLE_CLOUD_PROJECT}/g" instance.yml
 gcloud deployment-manager deployments create mc --config instance.yml
 IP=$(gcloud compute instances describe minecraft-vm --zone us-central1-f | grep natIP | cut -d\: -f2)
